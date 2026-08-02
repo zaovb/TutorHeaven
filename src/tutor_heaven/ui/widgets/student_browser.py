4ecddcccc9784fa9e8cc1d804dfde93603e5ce2e
Santiago Hernández <zaovbearolf@gmail.com>
2026-08-02 13:23:45 -0500

    Create student enrollments module

--- src/tutor_heaven/ui/widgets/student_browser.py
+++ src/tutor_heaven/ui/widgets/student_browser.py
@@ -1,15 +1,11 @@
 from PySide6.QtWidgets import (
-    QListWidget,
-    QPushButton,
     QSplitter,
     QVBoxLayout,
     QWidget,
 )
 
-from tutor_heaven.models.student_model import Student
 from tutor_heaven.ui.widgets.placeholder import Placeholder
-from tutor_heaven.ui.widgets.student_dialog import StudentDialog
-from tutor_heaven.ui.widgets.student_profile import StudentProfile
+from tutor_heaven.ui.widgets.student_enrollments import Students
 
 
 class StudentBrowser(QWidget):
@@ -18,51 +14,19 @@ class StudentBrowser(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
-        self.students: list[Student] = []
-
         layout = QVBoxLayout(self)
-
-        self.new_enrollment = QPushButton("➕ New Enrollment")
-        self.new_enrollment.clicked.connect(self.new_enrollment_clicked)
 
         self.splitter = QSplitter()
 
-        self.student_list = QListWidget()
-        self.student_list.currentRowChanged.connect(
-            self.student_selected
-        )
+        self.enrollments = Students()
 
-        self.right_widget = Placeholder(
+        self.placeholder = Placeholder(
             "Select a student\n\nto open the profile."
         )
 
-        self.splitter.addWidget(self.student_list)
-        self.splitter.addWidget(self.right_widget)
+        self.splitter.addWidget(self.enrollments)
+        self.splitter.addWidget(self.placeholder)
 
-        self.splitter.setSizes([250, 1000])
+        self.splitter.setSizes([700, 900])
 
-        layout.addWidget(self.new_enrollment)
         layout.addWidget(self.splitter)
-
-    def new_enrollment_clicked(self) -> None:
-        dialog = StudentDialog()
-
-        if not dialog.exec():
-            return
-
-        student = dialog.student
-
-        if student is None:
-            return
-
-        self.students.append(student)
-        self.student_list.addItem(student.name)
-
-    def student_selected(self, row: int) -> None:
-        if row < 0:
-            return
-
-        self.splitter.replaceWidget(
-            1,
-            StudentProfile(self.students[row]),
-        )
